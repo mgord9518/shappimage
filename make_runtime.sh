7e49058eaa37c0aa257cc6232ee9d9490ff0a3f4
Mathew Gordon <mgord9518@gmail.com>
2024-01-30 01:16:25 -0700

    Clean up
    runtime.sh:
     * Replace tabs with spaces
     * Now fetch squashfuse binaries from shImg header
    
    make_runtime.sh
     * No longer throws a bunch of errors for unused archs
     * Reduce some duplicated code and make it more readable

--- make_runtime.sh
+++ make_runtime.sh
@@ -6,7 +6,7 @@
 # ARCH variable sets the arch, COMP sets the compression algo
 # Default to all supported architectures and LZ4 compression if unset
 # TODO: Allow multiple compression algorithms in building
-[ -z $ARCH ] && ARCH='x86_64-aarch64-armhf'
+[ -z $ARCH ] && ARCH='x86_64-aarch64'
 [ -z $COMP ] && COMP='lz4'
 [ -z $img_type ] && img_type='squashfs'
 
@@ -24,21 +24,27 @@ else
     compress_flags="-9"
 fi
 
-for arch in 'x86_64' 'aarch64' 'x86' 'armhf'; do
+for arch in 'x86_64' 'aarch64' 'i386' 'armhf'; do
     # Download required squashfuse squashfusearies per architecture if they don't already
     # exist
     if [ $(grep "$arch" <<< "$ARCH") ]; then
         if [ ! -f "squashfuse/squashfuse.$arch"* ]; then
             wget "$squashfuse_source/squashfuse_${COMP}${static_prefix}.$arch" \
                 -O "squashfuse/squashfuse.$arch"
+
+            if [ $? -ne 0 ]; then
+                rm "squashfuse/squashfuse.$arch"
+                exit $?
+            fi
+
         fi
 
         if [ $COMPRESS_SQUASHFUSE ]; then
             "$compress_command" $compress_flags "squashfuse/squashfuse.$arch"
             rm "squashfuse/squashfuse.$arch"
-            binList="$binList squashfuse/squashfuse.$arch.gz"
+            bin_list="$bin_list squashfuse/squashfuse.$arch.gz"
         else
-            binList="$binList squashfuse/squashfuse.$arch"
+            bin_list="$bin_list squashfuse/squashfuse.$arch"
         fi
     fi
 done
@@ -62,41 +68,29 @@ sed -i "s/=_IMAGE_COMPRESSION_/=$COMP/" runtime
 sed -i "s/=_IMAGE_TYPE_/=$img_type/" runtime
 sed -i "s/=_ARCH_/='$arch'/" runtime
 
-# Sizes of all files being packed into the runtime
-_x64_l=$(printf "%07d" `wc -c squashfuse/squashfuse.x86_64* | cut -d ' ' -f 1`)
-i386_l=$(printf "%07d" `wc -c squashfuse/squashfuse.i386* | cut -d ' ' -f 1`)
-ar64_l=$(printf "%07d" `wc -c squashfuse/squashfuse.aarch64* | cut -d ' ' -f 1`)
-ar32_l=$(printf "%07d" `wc -c squashfuse/squashfuse.armhf* | cut -d ' ' -f 1`)
 
-# Offsets of squashfuse binaries by arch
-# These are used when the runtime is executed to know where in the file to
-# extract the appropriate binary
-_x64_o=$(cat runtime | wc -c | tr -dc '0-9')
-_x64_o=$(printf "%07d" $((10#$_x64_o + 1)))
-i386_o=$(printf "%07d" $((10#$_x64_o + 10#$_x64_l)))
-ar64_o=$(printf "%07d" $((10#$i386_o + 10#$i386_l)))
-ar32_o=$(printf "%07d" $((10#$ar64_o + 10#$ar64_l)))
+# Add one because this number is used by tail, which reads offsets off by 1
+offset=$(($(cat runtime | wc -c) + 1))
+length=0
 
-# Add in all the sizes and offsets
-sed -i "s/=_x64_o_/=$_x64_o/" runtime
-sed -i "s/=_x64_l_/=$_x64_l/" runtime
+# TODO: remove need for zero-padding
+for bin in $bin_list; do
+    offset=$(printf "%07d" $((10#$offset + 10#$length)))
+    length=$(printf "%07d" $(wc -c ${bin}* | cut -d ' ' -f 1))
 
-sed -i "s/=i386_o_/=$i386_o/" runtime
-sed -i "s/=i386_l_/=$i386_l/" runtime
+    arch=$(cut -d'.' -f2 <<< "$bin")
 
-sed -i "s/=ar64_o_/=$ar64_o/" runtime
-sed -i "s/=ar64_l_/=$ar64_l/" runtime
+    sed -i "s/${arch}_offset=0000000/${arch}_offset=$offset/" runtime
+    sed -i "s/${arch}_length=0000000/${arch}_length=$length/" runtime
+done
 
-sed -i "s/=ar32_o_/=$ar32_o/" runtime
-sed -i "s/=ar32_l_/=$ar32_l/" runtime
-
-runtime_size=$(cat runtime $binList | wc -c | tr -dc '0-9')
+runtime_size=$(cat runtime $bin_list | wc -c | tr -dc '0-9')
 
 # Had to expand to 7 digits because of DwarFS's large size
 image_offset=$(printf "%014d" "$runtime_size")
 sed -i "s/=_IMAGE_OFFSET_/=$image_offset/" runtime
 
-cat runtime $binList > runtime2
+cat runtime $bin_list > runtime2
 
 if [ ! $img_type = dwarfs ]; then
 	mv runtime2 "runtime-$COMP$STATIC-$ARCH"
@@ -104,3 +98,5 @@ else
 	mv runtime2 "runtime_dwarfs-static-$ARCH"
 	rm squashfuse/squashfuse.x86_64.gz
 fi
+
+rm runtime
--- runtime.sh
+++ runtime.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #.shImg.#
 
-# Copyright © 2021-2023 Mathew Gordon <github.com/mgord9518>
+# Copyright © 2021-2024 Mathew Gordon <github.com/mgord9518>
 
 # This is a proof-of-concept of a fat AppImage runtime using shell scripting
 # The base functionality is mostly modeled directly off of AppImage type 2's
@@ -53,9 +53,20 @@ image_compression=_IMAGE_COMPRESSION_
 sfs_offset=_IMAGE_OFFSET_
 image_offset=_IMAGE_OFFSET_
 
+# TODO: remove need for zero-padding
+x86_64_offset=0000000
+i386_offset=0000000
+armhf_offset=0000000
+aarch64_offset=0000000
+
+x86_64_length=0000000
+i386_length=0000000
+armhf_length=0000000
+aarch64_length=0000000
+
 # This will get switched to `y` if `add_integration.sh` is run
 contains_integration_zip=n
-version=0.4.0
+version=0.4.1
 arch=_ARCH_
 
 # Run these startup commands concurrently to make them faster
@@ -67,30 +78,30 @@ shell=$(readlink "/proc/$$/exe" &)
 wait
 
 if [ $TMPDIR ]; then
-	temp_dir="$TMPDIR"
+    temp_dir="$TMPDIR"
 elif [ -w "$XDG_RUNTIME_DIR" ]; then
-	temp_dir="$XDG_RUNTIME_DIR"
+    temp_dir="$XDG_RUNTIME_DIR"
 else
-	temp_dir="/tmp"
+    temp_dir="/tmp"
 fi
 
 # TODO: Add more ARMHF-compat arches
 if [ "$ARCH" = armv7l ]; then
-	ARCH=armhf
+    ARCH=armhf
 fi
 
 # Take advantage of non-POSIX features to minimize the amount of shelling out
 # to external programs, making the script faster
 case "$shell" in
 *bash)
-	use_bashisms=true
-	;;
+    use_bashisms=true
+    ;;
 *osh)
-	use_bashisms=true
-	;;
+    use_bashisms=true
+    ;;
 *zsh)
-	use_bashisms=true
-	;;
+    use_bashisms=true
+    ;;
 esac
 
 help_str='AppImage options:
@@ -116,297 +127,324 @@ unofficial AppImage runtime implemented in shell script
 # Flips the endianness of a supplied int by splitting every 2 chars onto a
 # seperate line, reversing those lines and removing the line breaks
 reverse_int_endianness() {
-	echo "$1" | sed -e 's/.\{2\}/&\n/g' | tac | tr -d '\n'
+    echo "$1" | sed -e 's/.\{2\}/&\n/g' | tac | tr -d '\n'
 }
 
 # Finds the length of an ELF or shImg file
 get_image_offset() {
-	if [ "$0" = "$TARGET_APPIMAGE" ]; then
-		echo $image_offset
-		return
-	fi
+    if [ "$0" = "$TARGET_APPIMAGE" ]; then
+        echo $image_offset
+        return
+    fi
 
-	elf_endianness=$(xxd -s 5 -l 1 -p "$TARGET_APPIMAGE" &)
-	elf_class=$(xxd -s 4 -l 1 -p "$TARGET_APPIMAGE" &)
-	wait
+    elf_endianness=$(xxd -s 5 -l 1 -p "$TARGET_APPIMAGE" &)
+    elf_class=$(xxd -s 4 -l 1 -p "$TARGET_APPIMAGE" &)
+    wait
 
-	# How to interpret integers based on their endianness
-	case "$elf_endianness" in
-	01) # Little endian
-		get_bytes() {
-			# Custom function instead of `-e` flag as it can't be expected to
-			# exist on non-GNU systems (eg: Alpine)
-			reverse_int_endianness $(xxd -s "$1" -l "$2" -p "$TARGET_APPIMAGE")
-		}
-		;;
-	02) # Big endian
-		get_bytes() {
-			xxd -s "$1" -l "$2" -p "$TARGET_APPIMAGE" 
-		}
-		;;
-	*)
-		1>&2 echo "invalid endianness (0x$elf_endianness), unable to find offset!"
-		exit 1
-		;;
-	esac
+    # How to interpret integers based on their endianness
+    case "$elf_endianness" in
+    01) # Little endian
+        get_bytes() {
+            # Custom function instead of `-e` flag as it can't be expected to
+            # exist on non-GNU systems (eg: Alpine)
+            reverse_int_endianness $(xxd -s "$1" -l "$2" -p "$TARGET_APPIMAGE")
+        }
+        ;;
+    02) # Big endian
+        get_bytes() {
+            xxd -s "$1" -l "$2" -p "$TARGET_APPIMAGE" 
+        }
+        ;;
+    *)
+        1>&2 echo "invalid endianness (0x$elf_endianness), unable to find offset!"
+        exit 1
+        ;;
+    esac
 
-	case "$elf_class" in
-	01) # 32 bit
-		shentsize='0x'$(get_bytes 46 2 &)
-		shnum='0x'$(get_bytes 48 2 &)
-		shoff='0x'$(get_bytes 32 4 &)
-		;;
-	02) # 64 bit
-		shentsize='0x'$(get_bytes 58 2 &)
-		shnum='0x'$(get_bytes 60 2 &)
-		shoff='0x'$(get_bytes 40 8 &)
-		;;
-	69) # shImg
-		echo $(get_var 'sfs_offset')
-		return
-		;;
-	*)
-		1>&2 echo "invalid or unknown class (0x$elf_class), unable to find offset!"
-		exit 1
-		;;
-	esac
+    case "$elf_class" in
+    01) # 32 bit
+        shentsize='0x'$(get_bytes 46 2 &)
+        shnum='0x'$(get_bytes 48 2 &)
+        shoff='0x'$(get_bytes 32 4 &)
+        ;;
+    02) # 64 bit
+        shentsize='0x'$(get_bytes 58 2 &)
+        shnum='0x'$(get_bytes 60 2 &)
+        shoff='0x'$(get_bytes 40 8 &)
+        ;;
+    69) # shImg
+        echo $(get_var 'sfs_offset')
+        return
+        ;;
+    *)
+        1>&2 echo "invalid or unknown class (0x$elf_class), unable to find offset!"
+        exit 1
+        ;;
+    esac
 
-	wait
+    wait
 
-	# This calculates the total length of an ELF file
-	echo $(($shnum * $shentsize + $shoff))
+    # This calculates the total length of an ELF file
+    echo $(($shnum * $shentsize + $shoff))
 }
 
 
 # Mount the filesystem image either using squashfuse/DwarFS on the host system
 # or by extracting an internal binary
 mount_appimage() {
-	# If AppDir instead of AppImage, return quickly
-	if [ -d "$TARGET_APPIMAGE" ] && [ -x "$TARGET_APPIMAGE/AppRun" ]; then
-		MNTDIR="$TARGET_APPIMAGE"
-		return
-	fi
+    # If AppDir instead of AppImage, return quickly
+    if [ -d "$TARGET_APPIMAGE" ] && [ -x "$TARGET_APPIMAGE/AppRun" ]; then
+        MNTDIR="$TARGET_APPIMAGE"
+        return
+    fi
 
-	case "$ARCH" in
-	x86_64)
-		offset=_x64_o_
-		length=_x64_l_
-		;;
-	i?86)
-		offset=i386_o_
-		length=i386_l_
-		;;
-	aarch64)
-		offset=ar64_o_
-		length=ar64_l_
-		;;
-	armhf)
-		offset=ar32_o_
-		length=ar32_l_
-		;;
-	*)
-		1>&2 echo "your machine architecture ($ARCH) is not supported by shImg version $version"
-		exit 1
-		;;
-	esac
+    case "$ARCH" in
+    x86_64)
+        offset=$x86_64_offset
+        length=$x86_64_length
+        ;;
+    i?86)
+        offset=$i386_offset
+        length=$i386_length
+        ;;
+    aarch64)
+        offset=$aarch64_offset
+        length=$aarch64_length
+        ;;
+    armhf)
+        offset=$armhf_offset
+        length=$armhf_length
+        ;;
+    *)
+        1>&2 echo "your machine architecture ($ARCH) is not supported by shImg version $version"
+        exit 1
+        ;;
+    esac
 
-	if [ $length -eq 0 ]; then
-		1>&2 echo "your machine architecture ($ARCH) is not supported in this bundle! :("
-		exit 1
-	fi
+    if [ $length -eq 0 ]; then
+        1>&2 echo "your machine architecture ($ARCH) is not supported in this bundle! :("
+        exit 1
+    fi
 
-	# Set variable for random numbers if not available in running shell
-	[ -z $RANDOM ] && RANDOM=$(tr -dc '0-9a-zA-Z' < /dev/urandom 2> /dev/null | head -c 8)
+    # Set variable for random numbers if not available in running shell
+    [ -z $RANDOM ] && RANDOM=$(tr -dc '0-9a-zA-Z' < /dev/urandom 2> /dev/null | head -c 8)
 
-	if [ !$use_bashisms ]; then
-		run_id="$(basename $TARGET_APPIMAGE | head -c 8)$RANDOM"
-	else
-		run_id="$(basename $TARGET_APPIMAGE)"
-		run_id="${run_id:0:8}$RANDOM"
-	fi
+    if [ !$use_bashisms ]; then
+        run_id="$(basename $TARGET_APPIMAGE | head -c 8)$RANDOM"
+    else
+        run_id="$(basename $TARGET_APPIMAGE)"
+        run_id="${run_id:0:8}$RANDOM"
+    fi
 
-	[ -z $MNTDIR ] && MNTDIR="$temp_dir/.mount_$run_id"
+    [ -z $MNTDIR ] && MNTDIR="$temp_dir/.mount_$run_id"
 
-	# Ensure that the AppImage exits gracefully and unmounts before the script
-	# exits
-	trap 'unmount_appimage 1' INT
+    # Ensure that the AppImage exits gracefully and unmounts before the script
+    # exits
+    trap 'unmount_appimage 1' INT
 
-	# Create the temporary and mounting directories if they don't exist
-	if [ ! -d "$temp_dir" ] && [ -w "$temp_dir/.." ]; then
-		mkdir -p "$temp_dir"
-	elif [ ! -d "$temp_dir" ] && [ ! -w "$temp_dir/.." ]; then
-		1>&2 echo "cannot create temporary directory $temp_dir! parent directory not writable!"
-	fi
+    # Create the temporary and mounting directories if they don't exist
+    if [ ! -d "$temp_dir" ] && [ -w "$temp_dir/.." ]; then
+        mkdir -p "$temp_dir"
+    elif [ ! -d "$temp_dir" ] && [ ! -w "$temp_dir/.." ]; then
+        1>&2 echo "cannot create temporary directory $temp_dir! parent directory not writable!"
+    fi
 
-	if [ ! -d "$MNTDIR" ] && [ -w "$temp_dir" ]; then
-		mkdir -p "$MNTDIR"
-	elif [ ! -w "$temp_dir" ]; then
-		1>&2 echo "failed to create mount dir! $temp_dir not writable!"
-		exit 1
-	fi
+    if [ ! -d "$MNTDIR" ] && [ -w "$temp_dir" ]; then
+        mkdir -p "$MNTDIR"
+    elif [ ! -w "$temp_dir" ]; then
+        1>&2 echo "failed to create mount dir! $temp_dir not writable!"
+        exit 1
+    fi
 
-	image_offset=$(get_image_offset)
-	extract_exe
+    image_offset=$(get_image_offset)
+    extract_exe
 
-	# Attempt to mount and throw an error if unsuccessful
-	mnt_cmd "$TARGET_APPIMAGE" "$MNTDIR" $image_offset
-	if [ $? -ne 0 ]; then
-		1>&2 echo "failed to mount bundle image! see error message above"
-		exit 1
-	fi
+    shImg_size=$(wc -c "$TARGET_APPIMAGE" | cut -d ' ' -f 1)
+    if [ $image_offset -ge $shImg_size ]; then
+        1>&2 echo "this runtime appears to have no application data appended"
+        1>&2 echo "notice: this runtime supports $image_type images compressed with $image_compression"
+        exit 1
+    fi
+
+    # Attempt to mount and throw an error if unsuccessful
+    mnt_cmd "$TARGET_APPIMAGE" "$MNTDIR" $image_offset
+    if [ $? -ne 0 ]; then
+        1>&2 echo "failed to mount bundle image! see error message above"
+        exit 1
+    fi
 }
 
 # Unmount and exit, prefering `fusermount` which is on practically all common 
 # desktop Linux distos, fall back on `umount` just in case
 # Lazy unmount is to fix "resource busy" problem when running on Ubuntu 18.04
 unmount_appimage() {
-	[ -d "$TARGET_APPIMAGE" ] && return
+    [ -d "$TARGET_APPIMAGE" ] && return
 
-	if command -v 'fusermount' > /dev/null; then
-		fusermount -uz "$MNTDIR" &
-	elif command -v 'fusermount3' > /dev/null; then
-		fusermount3 -uz "$MNTDIR" &
-	else
-		umount -l "$MNTDIR" &
-	fi
+    if command -v 'fusermount' > /dev/null; then
+        fusermount -uz "$MNTDIR" &
+    elif command -v 'fusermount3' > /dev/null; then
+        fusermount3 -uz "$MNTDIR" &
+    else
+        umount -l "$MNTDIR" &
+    fi
 
-	# Clean up all empty directories
-	rmdir "$temp_dir/.mount_"* 2> /dev/null &
+    # Clean up all empty directories
+    rmdir "$temp_dir/.mount_"* 2> /dev/null &
 
-	exit $1
+    exit $1
 }
 
 # Find the location of the internal binary (may be either squashfuse or DwarFS)
 # based on system arch
 extract_exe() {
-	temp_exe="$temp_dir/$UID-shImg-${image_type}-$image_compression-$ARCH"
+    temp_exe="$temp_dir/$UID-shImg-${image_type}-$image_compression-$ARCH"
 
-	if [ "$image_type" = 'squashfs' ]; then
-		command -v 'squashfuse' > /dev/null && temp_exe=$(command -v 'squashfuse')
+    if [ "$image_type" = 'squashfs' ]; then
+        mnt_cmd() {
+            "$temp_exe" "$1" "$2" -o offset=$3
+        }
+    elif [ "$image_type" = 'dwarfs' ]; then
+        command -v 'dwarfs' > /dev/null && temp_exe=$(command -v 'dwarfs')
 
-		mnt_cmd() {
-			"$temp_exe" "$1" "$2" -o offset=$3
-		}
-	elif [ "$image_type" = 'dwarfs' ]; then
-		command -v 'dwarfs' > /dev/null && temp_exe=$(command -v 'dwarfs')
+        mnt_cmd() {
+            "$temp_exe" "$1" "$2" -o offset=$3 -o debuglevel=error
+        }
+    fi
 
-		mnt_cmd() {
-			"$temp_exe" "$1" "$2" -o offset=$3 -o debuglevel=error
-		}
-	fi
+    # Don't extract it again if it's already there
+    [ -x "$temp_exe" ] && return
 
-	# Don't extract it again if it's already there
-	[ -x "$temp_exe" ] && return
-
-	# Extract it, make_runtime.sh will modify this adding a gzip extract into
-	# the pipe if `$COMPRESS_SQUASHFUSE` is set
-	tail -c +$offset "$0" | head -c $length > "$temp_exe" &
-	chmod 0700 "$temp_exe" &
-	wait
+    # Extract it, make_runtime.sh will modify this adding a gzip extract into
+    # the pipe if `$COMPRESS_SQUASHFUSE` is set
+    tail -c +$offset "$0" | head -c $length > "$temp_exe" &
+    chmod 0700 "$temp_exe" &
+    wait
 }
 
 get_var() {
-	grep -a -m 1 "$1=" "$TARGET_APPIMAGE" | cut -d '=' -f 2-
+    grep -a -m 1 "$1=" "$TARGET_APPIMAGE" | cut -d '=' -f 2-
 }
 
 # Handle AppImage-specific args (modeled after type 2 AppImage runtime)
 for i in "$@"; do
-	case "$i" in
-	--appimage-extract)
-		echo "Extracting"
-		if [ ! -d "$TARGET_APPIMAGE.appdir" ]; then
-			mkdir "$TARGET_APPIMAGE.appdir"
-			if [ $? -ne 0 ]; then
-				1>&2 echo "failed to create extraction directory! see error above"
-				exit 1
-			fi
-		elif [ ! -w "$TARGET_APPIMAGE.appdir" ]; then
-			1>&2 echo "extraction directory ($TARGET_APPIMAGE.appdir) isn't writable!"
-			exit 1
-		fi
+    case "$i" in
+    --appimage-extract)
+        echo "Extracting"
+        if [ ! -d "$TARGET_APPIMAGE.appdir" ]; then
+            mkdir "$TARGET_APPIMAGE.appdir"
+            if [ $? -ne 0 ]; then
+                1>&2 echo "failed to create extraction directory! see error above"
+                exit 1
+            fi
+        elif [ ! -w "$TARGET_APPIMAGE.appdir" ]; then
+            1>&2 echo "extraction directory ($TARGET_APPIMAGE.appdir) isn't writable!"
+            exit 1
+        fi
 
-		mount_appimage
-		cp -rv "$MNTDIR/." "$TARGET_APPIMAGE.appdir" | cut -d ' ' -f 3-
-		unmount_appimage 0
-		;;
-	--appimage-help)
-		echo "$help_str"
-		exit 0
-		;;
-	--appimage-mount)
-		mount_appimage
-		echo "$MNTDIR"
-		read REPLY
-		unmount_appimage 0
-		;;
-	--appimage-offset)
-		# Evaluate the image offset by adding nothing, this is to strip off
-		# leading zeroes
-		expr $(get_image_offset) + 0
-		exit 0
-		;;
-	--appimage-portable-home)
-		mkdir "$0.home"
+        # extract without requiring FUSE
+        # TODO: fix directory already exists error
+#        if [ "$image_type" = "squashfs" ]; then
+#            image_offset=$(get_image_offset)
+#            extract_exe
+#
+#            shImg_size=$(wc -c "$TARGET_APPIMAGE" | cut -d ' ' -f 1)
+#            if [ $image_offset -ge $shImg_size ]; then
+#                1>&2 echo "this runtime appears to have no application data appended"
+#                1>&2 echo "notice: this runtime supports $image_type images compressed with $image_compression"
+#                exit 1
+#            fi
+#
+#            "$temp_exe" "$TARGET_APPIMAGE" "$MNTDIR" \
+#                -o offset=$image_offset \
+#                -x \
+#                --extract-dest "$TARGET_APPIMAGE.appdir" \
+#                --verbose
+#
+#            exit 0
+#        fi
 
-		if [ $? -ne 0 ]; then
-			1>&2 echo "failed to create portable home! see error above"
-			exit 1
-		fi 
+        mount_appimage
+        cp -rv "$MNTDIR/." "$TARGET_APPIMAGE.appdir" | cut -d ' ' -f 3-
+        unmount_appimage 0
+        ;;
+    --appimage-help)
+        echo "$help_str"
+        exit 0
+        ;;
+    --appimage-mount)
+        mount_appimage
+        echo "$MNTDIR"
+        read REPLY
+        unmount_appimage 0
+        ;;
+    --appimage-offset)
+        # Evaluate the image offset by adding nothing, this is to strip off
+        # leading zeroes
+        expr $(get_image_offset) + 0
+        exit 0
+        ;;
+    --appimage-portable-home)
+        mkdir "$0.home"
 
-		echo "Created portable home at $0.home"
-		exit 0
-		;;
-	--appimage-portable-config)
-		mkdir "$0.config"
+        if [ $? -ne 0 ]; then
+            1>&2 echo "failed to create portable home! see error above"
+            exit 1
+        fi 
 
-		if [ $? -ne 0 ]; then
-			1>&2 echo "failed to create portable config! see error above"
-			exit 1
-		fi
+        echo "Created portable home at $0.home"
+        exit 0
+        ;;
+    --appimage-portable-config)
+        mkdir "$0.config"
 
-		echo "created portable config at $0.config"
-		exit 0
-		;;
-#	--appimage-signature)
-#		;;
-	--appimage-updateinfo)
-	# Prefer `unzip` it showed to be the fastest with my tests
-		if command -v 'unzip' > /dev/null; then
-			unzip -p "$TARGET_APPIMAGE" '.APPIMAGE_RESOURCES/update_info' | head -n 2 | tail -n 1
-		elif command -v 'bsdtar' > /dev/null; then
-			bsdtar -Oxf "$TARGET_APPIMAGE" '.APPIMAGE_RESOURCES/update_info' | head -n 2 | tail -n 1
-		else
-			# L O N G sed one-liner to extract the update information from the
-			# zip file placed at the end of the AppImage, this can be done
-			# because it's one of the few files that doesn't get compressed and
-			# has a special header and footer to make it easily locatable
-			tac "$TARGET_APPIMAGE" | sed -n '/---END APPIMAGE \[update_info\]---/,/---BEGIN APPIMAGE \[update_info\]---/{ /---.* APPIMAGE \[update_info\]---/d; p }'
-		fi
-		exit 0
-		;;
-	--appimage-version)
-		[ "$0" != "$TARGET_APPIMAGE" ] && version=$(get_var 'version')
-		echo "$version"
-		exit 0
-		;;
-	--appimage*)
-		1>&2 echo "$i is not implemented in version $version of shImg"
-		exit 1
-		;;
-	esac
+        if [ $? -ne 0 ]; then
+            1>&2 echo "failed to create portable config! see error above"
+            exit 1
+        fi
+
+        echo "created portable config at $0.config"
+        exit 0
+        ;;
+#    --appimage-signature)
+#        ;;
+    --appimage-updateinfo)
+    # Prefer `unzip` it showed to be the fastest with my tests
+        if command -v 'unzip' > /dev/null; then
+            unzip -p "$TARGET_APPIMAGE" '.APPIMAGE_RESOURCES/update_info' | head -n 2 | tail -n 1
+        elif command -v 'bsdtar' > /dev/null; then
+            bsdtar -Oxf "$TARGET_APPIMAGE" '.APPIMAGE_RESOURCES/update_info' | head -n 2 | tail -n 1
+        else
+            # L O N G sed one-liner to extract the update information from the
+            # zip file placed at the end of the AppImage, this can be done
+            # because it's one of the few files that doesn't get compressed and
+            # has a special header and footer to make it easily locatable
+            tac "$TARGET_APPIMAGE" | sed -n '/---END APPIMAGE \[update_info\]---/,/---BEGIN APPIMAGE \[update_info\]---/{ /---.* APPIMAGE \[update_info\]---/d; p }'
+        fi
+        exit 0
+        ;;
+    --appimage-version)
+        [ "$0" != "$TARGET_APPIMAGE" ] && version=$(get_var 'version')
+        echo "$version"
+        exit 0
+        ;;
+    --appimage*)
+        1>&2 echo "$i is not implemented in version $version of shImg"
+        exit 1
+        ;;
+    esac
 done
 
 # Done setting up, proceed to executing if no arguments are given
 mount_appimage
 
 if [ -d "$TARGET_APPIMAGE.home" ]; then
-	echo "setting \$HOME to $TARGET_APPIMAGE.home"
-	export HOME="$(realpath $TARGET_APPIMAGE.home)"
+    echo "setting \$HOME to $TARGET_APPIMAGE.home"
+    export HOME="$(realpath $TARGET_APPIMAGE.home)"
 fi
 
 if [ -d "$TARGET_APPIMAGE.config" ]; then
-	echo "setting \$XDG_CONFIG_HOME to $TARGET_APPIMAGE.config"
-	export XDG_CONFIG_HOME="$(realpath $TARGET_APPIMAGE.config)"
+    echo "setting \$XDG_CONFIG_HOME to $TARGET_APPIMAGE.config"
+    export XDG_CONFIG_HOME="$(realpath $TARGET_APPIMAGE.config)"
 fi
 
 export ARGV0="$TARGET_APPIMAGE" 
@@ -420,10 +458,10 @@ if [ -x "$MNTDIR/AppRun.$ARCH" ]; then
 elif [ -x "$MNTDIR/AppRun" ]; then
         "$MNTDIR/AppRun" "$@"
 
-		if [ $? = 127 ]; then
-			1>&2 echo "the application appears to be missing shared libraries and will not run :("
-			1>&2 echo "this most likely means that it wasn't packaged to work with your distro or libc version"
-		fi
+        if [ $? = 127 ]; then
+            1>&2 echo "the application appears to be missing shared libraries and will not run :("
+            1>&2 echo "this most likely means that it wasn't packaged to work with your distro or libc version"
+        fi
 elif [ -f "$MNTDIR/AppRun" ]; then
         1>&2 echo "AppRun found but isn't executable! please report this error to the developers of this application"
         # Exit in subshell to set $? variable
